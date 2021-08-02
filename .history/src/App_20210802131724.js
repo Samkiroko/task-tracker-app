b4db258615a26b2c6ce5acf333377286dc3939af
Samkiroko <kirokodev@gmail.com>
2021-08-02 13:38:44 +0300

    setup re-usable button

--- .history/src/App_20210802131724.js
+++ .history/src/App_20210802131724.js
@@ -2,8 +2,8 @@ import Header from './components/Header'
 
 function App() {
   return (
-    <div className='container'>
-      <Header title='Task Tracker' />
+    <div className='App'>
+      <Header />
     </div>
   )
 }
---
+++ .history/src/components/Button_20210802133542.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Button = ({ color, text }) => {
+  return (
+    <button style={{ backgroundColor: color }} className='btn'>
+      {text}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  color: 'steelblue',
+}
+
+export default Button
